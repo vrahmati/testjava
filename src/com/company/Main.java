c1b4171d14dd32a2233aa4ffb3db22a2dbc4176f
Rahmati <vahidrahmati@yahoo.com>
2018-04-04 16:47:50 +0430

    one commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,13 @@ package com.company;
 
 public class Main {
     private static final int i=0;
-
+    public static final int  j=10;
     public static void main(String[] args) {
 	// write your code here
+        String s="vahid";
+        System.out.println(s);
+
+
 
     }
 }
